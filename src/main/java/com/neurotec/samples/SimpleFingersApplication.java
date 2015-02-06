094f34b93bd4e74c23f534f4023adba742ac3937
vikaspandeysahaj <vikas.pandey06081986@gmail.com>
2015-02-06 15:12:30 +0530

    changed code 1

--- src/main/java/com/neurotec/samples/SimpleFingersApplication.java
+++ src/main/java/com/neurotec/samples/SimpleFingersApplication.java
@@ -9,38 +9,27 @@ import java.lang.reflect.InvocationTargetException;
 
 public class SimpleFingersApplication extends Applet {
 
-    public static void main(String[] arg){
-        JFrame frame = new JFrame();
-        frame.setTitle("Muzima Fingerprint Identification");
-        MainPanel panel = new MainPanel();
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        //frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
+    public void init() {
+        final Component parent = this;
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
 
-//    public void init() {
-//        final Component parent = this;
-//        try {
-//            SwingUtilities.invokeAndWait(new Runnable() {
-//                public void run() {
-//
-//                    JFrame frame = new JFrame();
-//                    frame.setTitle("Muzima Fingerprint Identification");
-//                    MainPanel panel = new MainPanel();
-//                    frame.add(panel);
-//                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                    frame.pack();
-//                    //frame.setLocationRelativeTo(null);
-//                    frame.setVisible(true);
-//                    add(frame);
-//                }
-//            });
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
-//    }
+                    JFrame frame = new JFrame();
+                    frame.setTitle("Muzima Fingerprint Identification");
+                    MainPanel panel = new MainPanel();
+                    frame.add(panel);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.pack();
+                    //frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                    add(frame);
+                }
+            });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 }
